4d35eb1baeb74403425c32d07cbf139b43bed600
Niels <niels.bresser@gmail.com>
2020-11-19 15:17:48 +0100

    Adding list and delete

---
+++ commands/deletevent.js
@@ -0,0 +1,37 @@
+const Event = require('../db/models/event')
+
+module.exports = {
+  name: 'deletevent',
+  description: 'Deleting entry in db',
+  async execute(message) {
+    const data = []
+
+    const input = message.content.slice(process.env.PREFIX.length).trim().split(/ +/)
+    const command = input.shift().toLowerCase()
+
+    const tagEvent = input[0]
+
+    if (!input.length) {
+      return message.channel.send(`You didn't provide any arguments, ${message.author}!`)
+    }
+
+    data.push(`Deleting event for:`)
+    data.push(`Event: ${tagEvent}`)
+
+    message.channel.send(data, { split: true })
+
+    try {
+      await Event.query().delete()
+      .where({ 
+        name: tagEvent
+        })
+      return message.reply(`Event "${tagEvent}" deleted.`)
+    } catch (e) {
+      console.log(e)
+      if (e.name === 'SequelizeUniqueConstraintError') {
+        return message.reply('That event is already deleted.')
+      }
+      return message.reply('Something went wrong with deleting the event.')
+    }
+  },
+}
