0eedcd1b905495f8af60bf6d8fe67605a7c8eaf8
Harry Parton <hello@harryparton.co.uk>
2020-11-20 23:40:10 +0100

    Bump back up to 5 minutes and use cached channels

--- scheduled/calendar.js
+++ scheduled/calendar.js
@@ -2,12 +2,12 @@ const persist = require('../src/persistantMessage')
 const calendar = require('../src/calendar')
 
 module.exports = {
-  name: 'events-test',
+  name: 'events-calendar',
   immediate: true,
   description: 'Update the calendar command every 5 minutes.',
-  format: '*/1 * * * *',
+  format: '*/5 * * * *',
   execute: async client => {
-    const infoChannel = await client.channels.fetch(`${process.env.INFO_CHANNEL || '772847261196877826'}`)
+    const infoChannel = await client.channels.cache.get(`${process.env.INFO_CHANNEL || '772847261196877826'}`)
     const data = await calendar.upcoming()
 
     let existingMessage = await persist.find(client, 'info-test')
